f4b2a0e4c3725b071f5716b9475d869df9e1285b
Christian Abdelmassih <chrabd@kth.se>
2017-08-18 10:42:45 +0200

    update app for new api

--- src/components/tinder/tinder-card/index.js
+++ src/components/tinder/tinder-card/index.js
@@ -5,11 +5,11 @@ import styles from './styles';
 
 class Card extends React.Component {
   render() {
-    let {image_url, name, likes} = this.props;
+    let {image, name, likes} = this.props;
     return (
       <View style={[styles.card]}>
         <View style={styles.canvas}>
-          <Image style={styles.image} source={{uri: image_url}}/>
+          <Image style={styles.image} source={{uri: image.tiny}}/>
           <View style={styles.textContainer}>
             <Text style={styles.text}>{name}</Text>
             <View style={styles.heartContainer}>
